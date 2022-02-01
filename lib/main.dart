b641afc396390fc57e5fc73fe90209455b867759
Anderson Mendes <developer.andersonmendes@gmail.com>
2022-01-31 22:31:25 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeWidget());
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+          child: Text(
+        'Anderson bonitão',
+        textDirection: TextDirection.ltr,
+      )),
+    );
+  }
+}
