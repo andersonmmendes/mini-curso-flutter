82041f550d1743024f5d457d651467c4e73631e3
Anderson Mendes <developer.andersonmendes@gmail.com>
2022-02-14 22:32:52 -0300

    Aula 13 - Finalizada

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/home_controller.dart';
 
-class HomeWidget extends StatelessWidget {
-  const HomeWidget({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    var controller = HomeController.of(context);
-
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: Center(child: Text('Anderson Mendes ${controller.value}')),
+      body: Center(child: Text('Anderson Mendes $counter')),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          controller.increment();
+          setState(() {
+            counter++;
+          });
         },
       ),
     );
--- lib/myapp.dart
+++ lib/myapp.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: HomeController(
-      child: const HomeWidget(),
+      child: const HomePage(),
     ));
   }
 }
